401398bf94734a8206083f9a02cfb762eabc6984
Kristian Engberg <krissoengberg@gmail.com>
2021-05-15 21:26:19 +0200

    added baseline for project
    Imported node modules i believe i will be using so far

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send({});
+});
+
+
+app.listen(8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 8080);
+});
